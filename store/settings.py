afc0337c5686dbac6f740341e78227a99b547136
AzizRasulov123 <unknown767689@gmail.com>
2026-02-08 13:53:09 +0500

    third commit

--- store/settings.py
+++ store/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'railway',
         'USER':'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+        'PASSWORD': os.environ['DB_PASSWORD_YO'],
         'HOST':'shinkansen.proxy.rlwy.net',
         'PORT':'29010'
 
